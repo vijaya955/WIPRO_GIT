9f8eaa6d7a38b2e27b407ffc4e08f521e9c3bdc1
vijaya955 <141250102+vijaya955@users.noreply.github.com>
2025-08-09 15:51:49 +0530

    Add files via upload

---
+++ EcomUserService/src/main/java/com/user/service/UserService.java
@@ -0,0 +1,38 @@
+package com.user.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.user.model.User;
+import com.user.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+    private UserRepository userRepository;
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow();
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
+
